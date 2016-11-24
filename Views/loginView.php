495c7159eecc846b3f78fe322795819de587f1b3
Tamonne Cotodie <TamonneC@dipar.local>
2016-11-24 16:10:42 +0200

    login form test added

---
+++ Views/loginView.php
@@ -0,0 +1,20 @@
+
+<html >
+
+<head>
+  <title>Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+<link href="css/loginCss.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<?php
+include_once("Views/includeFile.php");
+ ?>
+</head>
+<body>
+  <div >
+heloo
+  </div>
+
+</body>
+</html>
